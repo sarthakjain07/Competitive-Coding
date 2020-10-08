b0647d6f1d3b5d0d4cc89b45ae056a2c24a47eb8
Sarthak Jain <sarthakjain7@outlook.com>
2020-10-08 09:43:04 +0530

    Added Soution to question 2

---
+++ prepInsta_TCS/Q.2/main.py
@@ -0,0 +1,8 @@
+'''The int() function converts the specified value into an integer number.
+We are using the same int() method to convert the given input.
+int() accepts two arguments, number and base.
+Base is optional and the default value is 10.
+In the following program we are converting to base 17'''
+
+number=input()
+print(int(number,17))
