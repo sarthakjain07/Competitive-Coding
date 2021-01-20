cc1e67b3a646e33e0e0cdd620b882941205dd60b
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-20 08:41:27 +0530

    Working solution of Job sequencing problem using greedy algorithm

--- Greedy_Algorithm_Problems/Job_sequencing&deadlines.cpp
+++ Greedy_Algorithm_Problems/Job_sequencing&deadlines.cpp
@@ -1,49 +1,64 @@
 # include <iostream>
 # include <climits>
+# include <cstring>
 using namespace std;
 
-int main_max=INT_MAX;
+// int main_max=INT_MAX;
 int maxPosition(int * a, int size)
 {
     int max=0,max_po=0;
     for (int i = 0; i < size; i++)
     {
-        if(max<a[i] && a[i]<main_max)
+        if(max<a[i])
         {
             max=a[i];
             max_po=i;
         }
     }
-    main_max=max;
+    // main_max=max;
     return max_po;
 
 }
 
 int main()
 {
-    int size=7,main_size,max;
-    int *profit,*deadline,*Jobs,*a;
+    int size=7,main_size,max,profit_at_point;
+    int *profit,*deadline,*Jobs;
     profit=(int *)malloc(size*sizeof(int)); 
-    // deadline=(int *)malloc(size*sizeof(int));
+    deadline=(int *)malloc(size*sizeof(int));
     for (int i = 0; i < size; i++)
     {
         printf("Enter profit %d\n",i+1);
         scanf("%d",&profit[i]);
-        // printf("Enter deadline %d\n",i+1);
-        // scanf("%d",&deadline[i]);
+        printf("Enter deadline %d\n",i+1);
+        scanf("%d",&deadline[i]);
     }
     
-    // main_size=deadline[maxPosition(deadline,size)];
-    // Jobs=(int *)malloc(main_size*sizeof(int));
-    // a=profit;
-    for (int i = 0; i < size; i++)
+    main_size=deadline[maxPosition(deadline,size)];
+    Jobs=(int *)malloc(main_size*sizeof(int));
+    int j=main_size-1;
+    // initializing all elements to zero usinf memset function
+    memset(Jobs,0,main_size*sizeof(int));
+    int m;
+    for (int i = 0; i < main_size; i++)
     {
+
         max=maxPosition(profit,size);
-        // if(Jobs[deadline[max]-1]==0)
-        //     Jobs[deadline[max]-1]=a[max];
-        printf("%d\n",profit[max]);
+        m=deadline[max];
+        if(Jobs[m-1]==0)
+        {
+            Jobs[m-1]=profit[max];
+            j--;
+        }
+        else if(Jobs[j]==0 && m-1>=j)
+        {
+            Jobs[j]=profit[max];
+            j--;
+        }
+        profit[max]=-1;
     }
-    
+    for (int i = 0; i < main_size; i++)
+        cout<<Jobs[i]<<"\t";
     
      
     return 0;
